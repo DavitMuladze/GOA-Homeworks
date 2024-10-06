e154fd03147f53d33648d54f9d7dadf3f7515cff
DavitMuladze <datuna.muladze@gmail.com>
2024-10-06 11:45:22 -0700

    first commit

---
+++ day 0/classwork/classwork.py
@@ -0,0 +1,11 @@
+name = "my name is Dato"
+
+last_name = "my last name is Muladze"
+
+age = "i am 14 years old"
+
+height = 171
+
+country = "i live in Georgia"
+
+print(name + " " + last_name + " " + age + " " + country)
