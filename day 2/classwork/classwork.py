d9442ad87bf6a66ce4cd1191132276020acf6d07
niamuladze <niamuladze08@gmail.com>
2024-10-27 05:18:14 -0700

    commit

---
+++ day 2/classwork/classwork.py
@@ -0,0 +1,11 @@
+name = "my name is Dato"
+
+last_name = "my last name is Muladze"
+
+favcar = "and my favorite car is Nissan R33 skyline"
+
+age = "i am 14 years old"
+
+hobby = "my hobby is programming and playing basketball"
+
+print(name + " " + last_name + " " + age + " " + favcar + " " + hobby)
