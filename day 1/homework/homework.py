284e75ef444c6b947568ae4ad9c3cba8756d4195
niamuladze <niamuladze08@gmail.com>
2024-11-13 06:21:52 -0800

    commit

---
+++ day 1/homework/homework.py
@@ -0,0 +1,15 @@
+print("Davit Muladze")
+
+print(14)
+
+name = "Davit"
+
+surname = "Muladze"
+
+age = "14"
+
+hobby = "my hobby is programming and"
+
+favcar = "my fav car is BMW"
+
+print(name+ " "+surname+" "+age+" "+hobby+" "+favcar)
