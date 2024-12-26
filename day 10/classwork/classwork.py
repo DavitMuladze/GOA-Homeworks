b1b9bf54b9ee4330d0b5abc2f051082f516d739f
niamuladze <niamuladze08@gmail.com>
2024-12-26 08:16:17 -0800

    commit

---
+++ day 10/classwork/classwork.py
@@ -0,0 +1,20 @@
+name = input("Enter your name: ")
+
+print("my name is " " " + name)
+
+surname = input("enter your surname: ")
+
+print("my surname is" " " + surname)
+
+num1 = str(input("your age: "))
+
+print("i am" " " + num1)
+
+num2 = str(input("your birthday: "))
+
+print(num2)
+
+num1=str(input("enter your age: "))
+num2=str(input("enter your mothers age: "))
+
+print(num1 + num2)
---
+++ day 10/homework/homework.py
@@ -0,0 +1,22 @@
+# 1st homework
+name = input("Enter your name: ")
+
+print("hello, your name is " " " + name)
+
+age = input("enter your age: ")
+
+print("hello, your age is " " " + age)
+
+# 2nd homework
+
+num1 = str(input("sister's age is 17"))
+num2 = str(input("moms age is 43"))
+num3 = str(input("dads age is 53"))
+
+num1 = 17
+num2 = 43
+num3 = 53
+
+print(num1 + 10)
+print(num2 + 10)
+print(num3 + 10)
