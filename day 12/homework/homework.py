910b8d34bccd1c66b21d488f4c703b046d3cf0ff
niamuladze <niamuladze08@gmail.com>
2025-01-04 11:50:23 -0800

    commit

---
+++ day 12/homework/homework.py
@@ -0,0 +1,117 @@
+# 1st work
+
+num1 = 15  #Dato's Age
+num2 = 17  #Eka's Age
+num3 = 22  #Nika's Age
+
+print(num1 + num2 + num3)
+
+
+#2nd work
+
+age = 27
+print(age + 20)
+
+#3rd work
+
+#მეტობა - >
+
+num1 = 15 > 3 
+print(num1)
+
+num1 = 14 > 2
+print(num1)
+
+num1 = 19 > 18
+print(num1)
+
+num1 = 27 > 29
+print(num1)
+
+num1 = 12 > 13
+print(num1)
+
+#ნაკლებობა - <
+
+num1 = 15 < 3 
+print(num1)
+
+num1 = 15 < 17
+print(num1)
+
+num1 = 7 < 6
+print(num1)
+
+num1 = 17 < 23
+print(num1)
+
+num1 = 18 < 32
+print(num1)
+
+#მეტია ან ტოლია - >=
+
+num1 = 15 >= 3 
+print(num1)
+
+num1 = 17 >= 16
+print(num1)
+
+num1 = 15 >= 15
+print(num1)
+
+num1 = 5 >= 4
+print(num1)
+
+num1 = 21 >= 19
+print(num1)
+
+#ნაკლებია ან ტოლია - <=
+
+num1 = 15 <= 3 
+print(num1)
+
+num1 = 16 <= 14
+print(num1)
+
+num1 = 12 <= 11
+print(num1)
+
+num1 = 19 <= 23
+print(num1)
+
+num1 = 13 <= 7 
+print(num1)
+
+#ტოლობა - ==
+
+num1 = 15 == 3 
+print(num1)
+
+num1 = 16 == 16
+print(num1)
+
+num1 = 15 == 17 
+print(num1)
+
+num1 = 81 == 90 
+print(num1)
+
+num1 = 32 == 54 
+print(num1)
+
+#არ უდრის - !=
+
+num1 = 15 != 3 
+print(num1)
+
+num1 = 32 != 19 
+print(num1)
+
+num1 = 13 != 12 
+print(num1)
+
+num1 = 85 != 70 
+print(num1)
+
+num1 = 17 != 17
+print(num1)
