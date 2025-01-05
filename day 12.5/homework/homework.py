8c3a794f96f9dd3941ab275d79ae25439952f6da
niamuladze <niamuladze08@gmail.com>
2025-01-05 04:58:34 -0800

    commit

---
+++ day 12.5/homework/homework.py
@@ -0,0 +1,158 @@
+#1st work
+
+#მეტობა - >
+
+num1 = 15 > 3 
+print(num1)
+
+num1 = 14 > 2
+print(num1)
+
+num1 = 19 > 18
+print(num1)
+
+num1 = 27 > 29
+print(num1)
+
+num1 = 12 > 13
+print(num1)
+
+#ნაკლებობა - <
+
+num1 = 15 < 3 
+print(num1)
+
+num1 = 15 < 17
+print(num1)
+
+num1 = 7 < 6
+print(num1)
+
+num1 = 17 < 23
+print(num1)
+
+num1 = 18 < 32
+print(num1)
+
+#მეტია ან ტოლია - >=
+
+num1 = 15 >= 3 
+print(num1)
+
+num1 = 17 >= 16
+print(num1)
+
+num1 = 15 >= 15
+print(num1)
+
+num1 = 5 >= 4
+print(num1)
+
+num1 = 21 >= 19
+print(num1)
+
+#ნაკლებია ან ტოლია - <=
+
+num1 = 15 <= 3 
+print(num1)
+
+num1 = 16 <= 14
+print(num1)
+
+num1 = 12 <= 11
+print(num1)
+
+num1 = 19 <= 23
+print(num1)
+
+num1 = 13 <= 7 
+print(num1)
+
+#ტოლობა - ==
+
+num1 = 15 == 3 
+print(num1)
+
+num1 = 16 == 16
+print(num1)
+
+num1 = 15 == 17 
+print(num1)
+
+num1 = 81 == 90 
+print(num1)
+
+num1 = 32 == 54 
+print(num1)
+
+#არ უდრის - !=
+
+num1 = 15 != 3 
+print(num1)
+
+num1 = 32 != 19 
+print(num1)
+
+num1 = 13 != 12 
+print(num1)
+
+num1 = 85 != 70 
+print(num1)
+
+num1 = 17 != 17
+print(num1)
+
+#2nd work
+
+print(10 + 5)
+print(20 + 32)
+print(11 + 82)
+print(43 + 9)
+print(23 + 89)
+
+print(10 - 5)
+print(32 - 31)
+print(18 - 2)
+print(19 - 5)
+print(89 - 76)
+
+print(10 * 5)
+print(9 * 5)
+print(91 * 4)
+print(19 * 90)
+print(12 * 4)
+
+print(10 / 5)
+print(21 / 3)
+print(14 / 7)
+print(65 / 5)
+print(82 / 2)
+
+print(10 ** 5)
+print(17 ** 9)
+print(12 ** 29)
+print(92 ** 7)
+print(90 ** 72)
+
+print(10 % 5)
+print(12 % 89)
+print(10 % 6)
+print(121 % 62)
+print(12 % 51)
+
+#3rd work
+
+num1 = 20
+num2 = 5
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 // num2)
+
+#4th work
+
+#HTML-ში alt-ი გამოიყენება <img> თეგთან და წარმოადგენს სურათის ალტერნატიულ ტექსტს. 
+#ის ასრულებს რამდენიმე მნიშვნელოვან ფუნქციას მაგალითად:
+#1. სურათის აღწერა:როდესაც სურათი არ იტვირთება, ბრაუზერი აჩვენებს Alt ატრიბუტში მითითებულ ტექსტს სურათის ნაცვლად.
+#<img src="example.jpg" alt="ლამაზი ბუნება">
