eceef583f33a0e812941b9f901db275380223a99
DavitMuladze <datuna.muladze@gmail.com>
2025-04-10 13:05:00 -0700

    commit

---
+++ day 25/homework/homework.py
@@ -0,0 +1,24 @@
+#2.
+#Python-ში ფუნქციის განსაზღვრა ხდება სიტყვით def, რაც ნიშნავს define function — ფუნქციის განსაზღვრას.
+
+#def ფუნქციის_სახელი(პარამეტრები):
+    # კოდი, რომელიც უნდა შესრულდეს
+  #  return შედეგი
+
+#3.
+def combine_texts(Hello, world):
+    return Hello + world
+
+
+#4.
+def repeat_text(Hi, five):
+    return Hi * five
+
+#5.
+def greet(ზუკა):
+    return f"გამარჯობა, {ზუკა}!"
+
+#6.
+def format_words(words):
+    return f"{words[0]}, {words[1]} და {words[2]}"
+
